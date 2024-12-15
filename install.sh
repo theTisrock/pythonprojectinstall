dbc5465b6b26fc212aa96ac9295ed3752248710d
Chris Torok <torok.chris@gmail.com>
2024-12-15 15:46:39 -0500

    try return only

--- install.sh
+++ install.sh
@@ -73,7 +73,6 @@ then
   echo "Actual version: ${ACTUAL_PYTHON_VERSION}"
   echo "Stopping installation."
   return
-  exit
 fi
 echo "Your chosen Python version installed successfully!"
 echo "...done checking the correct version of Python."
